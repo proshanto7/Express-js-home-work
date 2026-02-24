baf8c1ce92e61ef4ccc7678c6a96d19221c9967e
proshanto7 <proshantosarker112@gmail.com>
2026-02-24 22:10:40 +0600

    project upload in github

---
+++ index.js
@@ -0,0 +1,43 @@
+const env = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 7000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DataBase connected");
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+
+const todo = mongoose.model("todolist", {
+  title: String,
+});
+// write data API Route
+app.post("/todo", async (req, res) => {
+  const createtask = new todo({
+    title: req.body.name,
+  });
+
+  await createtask.save();
+
+  res.status(200).send({
+    success: true,
+    message: "Task created successfully",
+    data: createtask,
+  });
+});
+
+// err meddileware
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send(err.message ? err.message : "Internal server Erros");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
