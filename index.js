4d69d5d47d841953c870e140424ab6b36e9b51a7
proshanto7 <proshantosarker112@gmail.com>
2026-02-26 02:57:04 +0600

    cors install

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const env = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = 7000;
 app.use(express.json());
-
+app.use(cors());
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
